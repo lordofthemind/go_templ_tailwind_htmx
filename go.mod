6aa3f16a528d593b24b519fb36997c2c3d40ec59
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2025-04-03 19:25:47 +0000

    working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lordofthemind/go_templ_tailwind_htmx
 
 go 1.24.0
+
+require github.com/a-h/templ v0.3.857 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/lordofthemind/go_templ_tailwind_htmx/templates"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Serve static files (CSS)
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Serve the Templ page
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := templates.IndexPage().Render(r.Context(), w)
+		if err != nil {
+			http.Error(w, "template rendering failed", http.StatusInternalServerError)
+		}
+	})
+
+	log.Println("Server running at http://localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", mux))
+}
